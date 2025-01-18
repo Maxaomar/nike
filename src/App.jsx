693fb23242c80321825376ef04e709eb7344fd9b
MOHAMED <ahmadiin612@gmail.com>
2025-01-18 22:59:10 +0300

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Services from "./sections/Services";
 import SpecialOffer from "./sections/SpecialOffer";
 import Subscribe from "./sections/Subscribe";
 import SuperQuality from "./sections/SuperQuality";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
          <main>
+          <Nav />
           <CustomersReviews />
           <Footer />
           <Hero />
